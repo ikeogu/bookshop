547d2870262468bc65391bb0296adb6f0eda18e0
ikeogu <ikeogu322@gmail.com>
2018-06-28 14:53:53 +0100

    change

--- includes/con_fig.php
+++ includes/con_fig.php
@@ -1,8 +1,8 @@
 <?php
 
     defined('DNS')? null :define ('DNS','mysql://b9d2fd4c849b93:2507b818@us-cdbr-iron-east-04.cleardb.net/heroku_30b1cca539f37c6?reconnect=true');
-	defined('DB_USER')? null: define('DB_USER','ikeogu322@gmail.com');
-	defined('DB_PASS')? null: define('DB_PASS', 'emmanuel12');
+	defined('DB_USER')? null: define('DB_USER','b9d2fd4c849b93');
+	defined('DB_PASS')? null: define('DB_PASS', '2507b818');
 
 
 ?>

a9ef1c1144e4d19c16ae16c970cb61aa6bb78cce
ikeogu <ikeogu322@gmail.com>
2018-06-28 15:37:06 +0100

    change

--- includes/con_fig.php
+++ includes/con_fig.php
@@ -1,8 +1,8 @@
 <?php
 
-    defined('DNS')? null :define ('DNS','mysql://b9d2fd4c849b93:2507b818@us-cdbr-iron-east-04.cleardb.net/heroku_30b1cca539f37c6?reconnect=true');
-	defined('DB_USER')? null: define('DB_USER','b9d2fd4c849b93');
-	defined('DB_PASS')? null: define('DB_PASS', '2507b818');
+    defined('DNS')? null :define ('DNS','mysql://bdf8fc6363be33:538d622b@us-cdbr-iron-east-04.cleardb.net/heroku_d9acf15f3ed66ba?reconnect=true;dbname=heroku_d9acf15f3ed66ba;charset=utf8');
+	defined('DB_USER')? null: define('DB_USER','bdf8fc6363be33');
+	defined('DB_PASS')? null: define('DB_PASS', '538d622b');
 
 
 ?>

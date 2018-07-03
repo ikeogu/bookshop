13466d8b38e3dd690c50dda09f82ff6d91ad794b
ikeogu <ikeogu322@gmail.com>
2018-07-03 05:14:51 +0100

    new commit

--- includes/con_fig.php
+++ includes/con_fig.php
@@ -1,9 +1,10 @@
 <?php
 
-    defined('DNS')? null :define ('DNS','mysql://bdf8fc6363be33:538d622b@us-cdbr-iron-east-04.cleardb.net/heroku_d9acf15f3ed66ba?reconnect=true;dbname=heroku_d9acf15f3ed66ba;charset=utf8');
-	defined('DB_USER')? null: define('DB_USER','bdf8fc6363be33');
-	defined('DB_PASS')? null: define('DB_PASS', '538d622b');
+    defined('DNS')? null :define ('DNS','mysql://b9ebf992f7d79d:4ba61da5@us-cdbr-iron-east-04.cleardb.net/heroku_214c4c305ad6511?reconnect=true;dbname=heroku_214c4c305ad6511;charset=utf8');
+	defined('DB_USER')? null: define('DB_USER','b9ebf992f7d79d');
+	defined('DB_PASS')? null: define('DB_PASS', '4ba61da5');
 
 
 ?>
       
+:

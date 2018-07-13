345cc0f5c1ffbaac3a99f4a3471e76ab8b81eefb
ikeogu <ikeogu322@gmail.com>
2018-07-13 08:57:56 +0100

    little change

--- includes/con_fig.php
+++ includes/con_fig.php
@@ -1,8 +1,8 @@
 <?php
 
-    defined('DNS')? null :define ('DNS','mysql:host=localhost;dbname=heldy_products;charset=utf8');
-	defined('DB_USER')? null: define('DB_USER','root');
-	defined('DB_PASS')? null: define('DB_PASS', '');
+    defined('DNS')? null :define ('DNS','mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_29e54096f9ec213;charset=utf8');
+	defined('DB_USER')? null: define('DB_USER','b1ce44be8672d4');
+	defined('DB_PASS')? null: define('DB_PASS', '9d04f95c');
 
 
 ?>
